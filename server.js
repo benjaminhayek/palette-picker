1a56430084315df3de9f53e007e5aa75ef34334c
bhayek12@gmail.com <bhayek12@gmail.com>
2018-11-28 19:25:56 -0700

    Add posts and gets for palettes and projects

--- server.js
+++ server.js
@@ -28,41 +28,59 @@ app.get('/api/v1/palette_projects', (request, response) => {
       })
 })
 
-// app.post('/api/v1/papers', (request, reponse) => {
-//   const paper = request.body
+app.post('/api/v1/palette_projects', (request, response) => {
+  const project = request.body
 
-//   for(let requiredParam of ['title', 'author']) {
-//       if(!paper[requiredParam]) {
-//           response.status(422).json({error: error.message})
-//       }
-//   }
+  for(let requiredParam of ['name']) {
+      if(!project[requiredParam]) {
+          response.status(422).json({error: error.message})
+      }
+  }
 
-//   database('papers').insert(paper, 'id')
-//       .then(paperIds => {
-//           response.status(201).json({id: paperIds[0]})
-//       })
-//       .catch(error => {
-//           response.status(500).json({error: error.message})
-//       })
-// })
+  database('palette_projects').insert(project, 'id')
+      .then(projectIds => {
+          response.status(201).json({id: projectIds[0]})
+      })
+      .catch(error => {
+          response.status(500).json({error: error.message})
+      })
+})
 
-// app.get('/api/v1/papers/:id', (request, response) => {
-//   const { id } = request.params
+app.get('/api/v1/palette_projects/:id', (request, response) => {
+  const { id } = request.params
   
-//   database('papers').where('id', id).select()
-//       .then(paper => response.status(200).json(paper))
-//       .catch(error => console.log(`Error fetching paper: ${error.message}`))
-// })
+  database('palette_projects').where('id', id).select()
+      .then(project => response.status(200).json(project))
+      .catch(error => console.log(`Error fetching project: ${error.message}`))
+})
 
-// app.get('/api/v1/footnotes', (request, response) => {
-//   database('footnotes').select()
-//       .then(footnotes => {
-//           response.status(200).json(footnotes)
-//       })
-//       .catch(error => {
-//           response.status(500).json({ error: error.message });
-//       })
-// })
+app.get('/api/v1/palettes', (request, response) => {
+  database('palettes').select()
+      .then(palettes => {
+          response.status(200).json(palettes)
+      })
+      .catch(error => {
+          response.status(500).json({ error: error.message });
+      })
+})
+
+app.post('/api/v1/palette_projects/:project_id/palettes', (request, response) => {
+  const palette = request.body
+
+  for(let requiredParam of ['palette', 'color1', 'color2', 'color3', 'color4', 'color5']) {
+      if(!palette[requiredParam]) {
+          response.status(422).json({error: error.message})
+      }
+  }
+
+  database('palettes').insert(palette, 'id')  
+      .then(palettes => {
+          response.status(201).json(palettes)
+      })
+      .catch(error => {
+          response.status(500).json({error: error.message})
+      })
+})
 
 app.listen(app.get('port'), () => {
     console.log(`${app.locals.title} is running on ${app.get('port')}.`);
