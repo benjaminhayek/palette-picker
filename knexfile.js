18fe1b471a3a0219e576cf1400796b591bce8ee4
bhayek12@gmail.com <bhayek12@gmail.com>
2018-11-29 14:13:51 -0700

    Change knex file

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,23 @@
 // Update with your config settings.
 
 module.exports = {
-
-  development: {
-    client: 'pg',
-    connection: 'postgres://localhost/projects',
-    migrations: {
-      directory: './db/migrations'
-    },
-    seeds: {
-      directory: './db/seeds/dev'
-    },
-    production: {
-      client: 'pg',
-      connection: process.env.DATABASE_URL + `?ssl=true`,
-      migrations: {
-        directory: './db/migrations'
-      },
-      useNullAsDefault: true
-    }
-  }
+ development: {
+   client: 'pg',
+   connection: 'postgres://localhost/palettepicker',
+   migrations: {
+     directory: './db/migrations',
+   },
+   seeds: {
+     directory: './db/seeds/dev',
+   },
+   useNullAsDefault: true,
+ },
+ production: {
+   client: 'pg',
+   connection: process.env.DATABASE_URL + `?ssl=true`,
+   migrations: {
+     directory: './db/migrations',
+   },
+   useNullAsDefault: true,
+ },
 };
